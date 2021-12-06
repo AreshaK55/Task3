f9e54cbae07c939e4d9bf2ec0e25a34320c6fa03
areshak55 <areshak55@gmail.com>
2021-12-06 14:42:50 +0500

    New Activity

---
+++ app/src/main/java/com/example/task3_520/Acitivity5.java
@@ -0,0 +1,46 @@
+package com.example.task3_520;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity5 extends AppCompatActivity {
+
+    TextView tvScore;
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5);
+
+        tvScore = findViewById(R.id.showScore);
+        btn1 = findViewById(R.id.btn1);
+
+        Bundle extras = getIntent().getExtras();
+        String value = extras.getString("key");
+
+        tvScore.setText(value);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, "Thank You For Existing...!!!");
+                sendIntent.setType("text/plain");
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
+
+            }
+        });
+
+
+    }
+}
